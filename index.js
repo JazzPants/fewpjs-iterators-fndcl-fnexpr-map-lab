87ea136cb39119014f72a1067556d57a573c91ac
JazzPants <leighton.l14@gmail.com>
2021-11-19 00:49:04 +1100

    Done!

--- index.js
+++ index.js
@@ -13,5 +13,15 @@ const tutorials = [
 
 const titleCased = () => {
   // use the .map method on the tutorials to return a new array
-  return tutorials
+  return tutorials.map(tutorial => {
+    const words = tutorial.split(' ') //for each string, with any words or numbers separated by spaces, creates an array with those words as elements
+    const capitalizeWord = words.map(word => word.charAt(0).toUpperCase() + word.slice(1)); //make first letter capital of each word, then separately slice the word and add it to the capital letter
+    const newTutorials = capitalizeWord.join(' ');//joins each element in a string, with each string separated by ' '
+    return newTutorials;
+  })
 }
+
+//need curly brackets for outermost arrow fn because we have more than one operation to perform.
+//i.e. there is more than one expresison, we need curly brackets and a return
+
+console.log(titleCased());
